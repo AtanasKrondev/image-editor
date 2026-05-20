5ed5d9c281b951fad584a9f76553bad7f16c45a7
Atanas Krondev <krondev@gmail.com>
2026-05-20 19:28:19 +0300

    feat: init expressjs app

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT ?? 5000
+
+app.get('/health', (_req, res) => {
+  res.json({ status: 'ok' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
